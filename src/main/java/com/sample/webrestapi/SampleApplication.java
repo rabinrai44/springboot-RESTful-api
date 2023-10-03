4751ade2700c467edd0db70d82ebb837d80afe2f
Rabin Rai <rabinrai44@gmail.com>
2023-10-03 00:56:09 -0400

    Enabled and setup web security

--- src/main/java/com/sample/webrestapi/SampleApplication.java
+++ src/main/java/com/sample/webrestapi/SampleApplication.java
@@ -2,9 +2,8 @@ package com.sample.webrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class SampleApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sample/webrestapi/config/WebSecurity.java
+++ src/main/java/com/sample/webrestapi/config/WebSecurity.java
@@ -2,10 +2,32 @@ package com.sample.webrestapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurity {
+
+    private static final String PUBLIC_URLS = "";
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable()).cors((crs) -> crs.disable())
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(HttpMethod.GET, PUBLIC_URLS).permitAll()
+                        .requestMatchers(HttpMethod.POST, PUBLIC_URLS).permitAll()
+                        .requestMatchers(HttpMethod.PUT, PUBLIC_URLS).permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/user/delete/**").hasAnyAuthority("DELETE:USER")
+                        .requestMatchers(HttpMethod.DELETE, "/customer/delete/**").hasAnyAuthority("DELETE:CUSTOMER")
+                        .anyRequest().authenticated())
+                .exceptionHandling((exception) -> exception.accessDeniedHandler(null).authenticationEntryPoint(null));
+
+        return http.build();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
