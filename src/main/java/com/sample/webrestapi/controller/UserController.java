22db11b6f2035c565c681a868c18be136c8017af
Rabin Rai <rabinrai44@gmail.com>
2023-10-06 22:10:26 -0400

    Refactored the auth endpoints

--- src/main/java/com/sample/webrestapi/controller/UserController.java
+++ src/main/java/com/sample/webrestapi/controller/UserController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sample.webrestapi.dto.AppUserAdd;
-import com.sample.webrestapi.dto.AppUserDto;
 import com.sample.webrestapi.dto.LoginRequest;
 import com.sample.webrestapi.model.AppUser;
 import com.sample.webrestapi.model.HttpResponse;
@@ -27,60 +26,59 @@ import com.sample.webrestapi.service.UserService;
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
-    private static final Logger logger = (Logger) LogManager.getLogger(UserController.class);
+        private static final Logger logger = (Logger) LogManager.getLogger(UserController.class);
 
-    @Autowired
-    private UserService userService;
+        @Autowired
+        private UserService userService;
 
-    @Autowired
-    private AuthenticationManager authManager;
+        @Autowired
+        private AuthenticationManager authManager;
 
-    @PostMapping("/login")
-    public ResponseEntity<HttpResponse> login(@RequestBody LoginRequest request) {
-        var result = authManager
-                .authenticate(new UsernamePasswordAuthenticationToken(request.email(), request.password()));
+        @PostMapping("/login")
+        public ResponseEntity<HttpResponse> login(@RequestBody LoginRequest request) {
+                var result = authManager
+                                .authenticate(new UsernamePasswordAuthenticationToken(request.email(),
+                                                request.password()));
+                return ResponseEntity.ok()
+                                .body(
+                                                HttpResponse.builder()
+                                                                .timeStamp(getTimestamp())
+                                                                .data(Map.of("authenticated", result.isAuthenticated()))
+                                                                .message("User logged in successfully")
+                                                                .status(HttpStatus.OK)
+                                                                .statusCode(HttpStatus.OK.value())
+                                                                .build());
+        }
 
-        logger.info("User was logged in successfully.");
+        @PostMapping("/register")
+        public ResponseEntity<HttpResponse> saveUser(@RequestBody AppUserAdd userDto) {
+                AppUser user = new AppUser();
+                user.setFirstName(userDto.getFirstName());
+                user.setLastName(userDto.getLastName());
+                user.setEmail(userDto.getEmail());
+                user.setPassword(userDto.getPassword());
+                user.setPhone(userDto.getPhone());
+                user.setTitle(userDto.getTitle());
+                user.setBio(userDto.getBio());
+                user.setImageUrl(userDto.getImageUrl());
+                userService.createUser(user);
 
-        return ResponseEntity.ok()
-                .body(
-                        HttpResponse.builder()
-                                .timeStamp(LocalDateTime.now().toString())
-                                .data(Map.of("authenticated", result.isAuthenticated()))
-                                .message("User logged in successfully")
-                                .status(HttpStatus.OK)
-                                .statusCode(HttpStatus.OK.value())
-                                .build());
-    }
+                return ResponseEntity.created(getURI("/api/v1/user/login"))
+                                .body(
+                                                HttpResponse.builder()
+                                                                .timeStamp(getTimestamp())
+                                                                .message("User created successfully")
+                                                                .status(HttpStatus.CREATED)
+                                                                .statusCode(HttpStatus.CREATED.value())
+                                                                .build());
+        }
 
-    @PostMapping("/register")
-    public ResponseEntity<HttpResponse> saveUser(@RequestBody AppUserAdd userDto) {
-        AppUser user = new AppUser();
-        user.setFirstName(userDto.getFirstName());
-        user.setLastName(userDto.getLastName());
-        user.setEmail(userDto.getEmail());
-        user.setPassword(userDto.getPassword());
-        user.setPhone(userDto.getPhone());
-        user.setTitle(userDto.getTitle());
-        user.setBio(userDto.getBio());
-        user.setImageUrl(userDto.getImageUrl());
+        private URI getURI(String path) {
+                return URI.create(
+                                ServletUriComponentsBuilder.fromCurrentContextPath().path(path).toUriString());
+        }
 
-        AppUserDto respo = userService.createUser(user);
-
-        logger.info("User was created successfully.");
-
-        return ResponseEntity.created(getURI())
-                .body(
-                        HttpResponse.builder()
-                                .timeStamp(LocalDateTime.now().toString())
-                                .data(Map.of("user", respo))
-                                .message("User created successfully")
-                                .status(HttpStatus.CREATED)
-                                .statusCode(HttpStatus.CREATED.value())
-                                .build());
-    }
-
-    private URI getURI() {
-        return URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/login").toUriString());
-    }
+        private String getTimestamp() {
+                return LocalDateTime.now().toString();
+        }
 }
